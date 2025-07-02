d0455e884f9dac8d3fca03d9b06a84f3a9295e62
Douglas Hernandes <douglashernandes@icloud.com>
2025-07-01 22:23:03 -0300

    criando tabela para posts request

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-document.addEventListener("DOMContentLoader",);
+async function testaApi(){
+    const response = await fetch("http://localhost:3000/posts");
+    const posts = await response.json(); 
+    
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tbody = document.createElement("tbody");
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    document.getElementById("tabela-api").appendChild(table);
+
+    posts.forEach(item => {
+        const tr = document.createElement("tr");
+
+        const td1 = document.createElement("td");
+        const id = item.id;
+        td1.innerHTML = id;
+        tr.appendChild(td1);
+
+        const td2 = document.createElement("td");
+        const title = item.title;
+        td2.innerHTML = title;
+        tr.appendChild(td2);
+
+        const td3 = document.createElement("td");
+        const views = item.views;
+        td3.innerHTML = views;
+        tr.appendChild(td3);
+
+        tbody.appendChild(tr);
+    });
+}
+document.addEventListener("DOMContentLoader", testaApi());
