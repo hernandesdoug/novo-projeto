606ff1ed5f16b0e339c6e57eb7394219480dfae6
Douglas Hernandes <douglashernandes@icloud.com>
2025-07-07 16:35:22 -0300

    funcoes de alterar/deletar registro json

--- js/post.js
+++ js/post.js
@@ -9,3 +9,19 @@ async function novoRegistro(event){
     console.log(response);
     console.log(await response.json() );
 }
+async function alteraRegistro(event){
+    event.preventDefault();
+    const response = await fetch("http://localhost:3000/posts/ebba", { 
+        method:"put",
+        body:JSON.stringify({title: "novo titulo", views: 150})
+    });
+    console.log(response);
+}
+async function deletaRegistro(event){
+    event.preventDefault();
+    const response = await fetch("http://localhost:3000/posts/ebba", { 
+        method:"delete",
+        body:JSON.stringify({title, views})
+    });
+    console.log(response);
+}
