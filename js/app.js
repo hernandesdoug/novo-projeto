4e7354fdb41a642dbc4e3d5a74e252f20c8c0263
Douglas Hernandes <douglashernandes@icloud.com>
2025-08-06 17:58:21 -0300

    alteracao para criar campos de input no js

--- js/app.js
+++ js/app.js
@@ -1,51 +1,49 @@
 let tabelaExiste = false;
 async function testaApi(){
     if (!tabelaExiste) {
-        const response = await fetch("http://localhost:3333/usuarios");
-        const usuarios = await response.json(); 
-        montaUsuarios(usuarios);
+        const [responseUsuarios, responseEmpresas] = await Promise.all([
+            await fetch("http://localhost:3333/usuarios"),
+            await fetch("http://localhost:3333/empresas")
+        ])  
+        const [usuarios, empresas] = await Promise.all([
+            await responseUsuarios.json(),
+            await responseEmpresas.json(),
+        ])  
+        montaTabela(usuarios, 'usuarios');
     
-        const respComments = await fetch("http://localhost:3333/empresas");
-        const empresas = await respComments.json(); 
-        montaEmpresas(empresas);
+        montaTabela(empresas, 'empresas');
 
         tabelaExiste = true;
     }
 }
 
-function montaUsuarios(usuarios) {
+function montaTabela(titulo, tabela) {
     const table = document.createElement("table");
     const thead = document.createElement("thead");
     const tbody = document.createElement("tbody");
-    cabecalho(thead, 'id');
-    cabecalho(thead, 'nome');
-    cabecalho(thead, 'email');
-    cabecalho(thead, 'Ações');
-    table.appendChild(thead);   
-    table.appendChild(tbody);
-    document.getElementById("tabela-posts").appendChild(table);
+    
 
-    usuarios.forEach(item => {
+    if (tabela == 'usuarios') {
+        cabecalho(thead, 'id');
+        cabecalho(thead, 'nome');
+        cabecalho(thead, 'email');
+        cabecalho(thead, 'Ações');
+        table.appendChild(thead); 
+        table.appendChild(tbody);
+        document.getElementById("tabela-usuario").appendChild(table);
+    } else {
+        cabecalho(thead, 'id');
+        cabecalho(thead, 'empresa');
+        cabecalho(thead, 'usuarioID');
+        cabecalho(thead, 'Ações');
+        table.appendChild(thead); 
+        table.appendChild(tbody);
+        document.getElementById("tabela-empresa").appendChild(table);
+    }
+
+    titulo.forEach(item => {
         const dados = item;
-        linhaUsuarios(tbody, dados);
-    });
-}
-
-function montaEmpresas(empresas) {
-    const table = document.createElement("table");
-    const thead = document.createElement("thead");
-    const tbody = document.createElement("tbody");
-    cabecalho(thead, 'id');
-    cabecalho(thead, 'nome Empresa');
-    cabecalho(thead, 'usuarioId');
-    cabecalho(thead, 'Ações');
-    table.appendChild(thead);   
-    table.appendChild(tbody);
-    document.getElementById("tabela-comments").appendChild(table);
-
-    empresas.forEach(item => {
-        const dados = item;
-        linhaEmpresas(tbody, dados);
+        linhaTabela(tbody, dados, tabela);
     });
 }
 
@@ -55,48 +53,31 @@ function cabecalho(thead, titulo){
     thead.appendChild(th);
 }
 
-function linhaUsuarios(tbody, dados){
+function linhaTabela(tbody, dados, tabela){
     const tr = document.createElement("tr");
     const td1 = document.createElement("td");
     td1.innerHTML = dados.id;
     tr.appendChild(td1);
 
     const td2 = document.createElement("td");
-    td2.innerHTML = dados.nome;
+    if (tabela == 'usuarios') {
+        td2.innerHTML = dados.nome;
+    } else {
+        td2.innerHTML = dados.nomeEmpresa; 
+    }
     tr.appendChild(td2);
 
-
     const td3 = document.createElement("td");
-    td3.innerHTML = dados.email;
+    if (tabela == 'usuarios') {
+        td3.innerHTML = dados.email;
+    }else {
+        td3.innerHTML = dados.usuarioId;
+    }
     tr.appendChild(td3);
 
     const td4 = document.createElement("td");
     const a = document.createElement("a");
-    a.setAttribute("href", `post.html?id=${dados.id}&tipo=usuarios`);
-    a.innerHTML = "editar";
-    td4.appendChild(a);
-    tr.appendChild(td4);
-
-    tbody.appendChild(tr);
-}
-function linhaEmpresas(tbody, dados){
-    const tr = document.createElement("tr");
-    const td1 = document.createElement("td");
-    td1.innerHTML = dados.id;
-    tr.appendChild(td1);
-
-    const td2 = document.createElement("td");
-    td2.innerHTML = dados.nomeEmpresa;
-    tr.appendChild(td2);
-
-    const td3 = document.createElement("td");
-    td3.innerHTML = dados.usuarioId;
-    tr.appendChild(td3);
-
-    const td4 = document.createElement("td");
-    const a = document.createElement("a");
-    
-    a.setAttribute("href", `post.html?id=${dados.id}&tipo=empresas`);
+    a.setAttribute("href", `post.html?id=${dados.id}&tipo=${tabela}`);
     a.innerHTML = "editar";
     td4.appendChild(a);
     tr.appendChild(td4);
--- js/post.js
+++ js/post.js
@@ -1,31 +1,141 @@
 let id = '';
 let tipo = '';
+
+function montaFormulario(tipo) {
+    const formulario = document.createElement("form");
+    if (tipo == 'usuarios') {
+        const labelNome = document.createElement("label");
+        labelNome.setAttribute("for", "nome");
+        labelNome.textContent = "Nome: ";
+
+        const nomeInput = document.createElement("input");
+        nomeInput.setAttribute("type", "text");
+        nomeInput.setAttribute("id", "nome");
+        nomeInput.setAttribute("name", "nome");
+
+        const labelEmail = document.createElement("label");
+        labelEmail.setAttribute("for", "email");
+        labelEmail.textContent = "Email: ";
+
+        const emailInput = document.createElement("input");
+        emailInput.setAttribute("type", "text");
+        emailInput.setAttribute("id", "email");
+        emailInput.setAttribute("name", "email");
+
+        const labelSenha = document.createElement("label");
+        labelSenha.setAttribute("for", "senha");
+        labelSenha.textContent = "Senha: ";
+
+        const senhaInput = document.createElement("input");
+        senhaInput.setAttribute("type", "text");
+        senhaInput.setAttribute("id", "senha");
+        senhaInput.setAttribute("name", "senha");
+        formulario.appendChild(labelNome);
+        formulario.appendChild(nomeInput);
+        formulario.appendChild(labelEmail);
+        formulario.appendChild(emailInput);
+        formulario.appendChild(labelSenha);
+        formulario.appendChild(senhaInput);
+        document.getElementById("container").appendChild(formulario);
+    }
+
+    if (tipo == 'empresas') {
+        const labelEmpresa = document.createElement("label");
+        labelEmpresa.setAttribute("for", "empresa");
+        labelEmpresa.textContent = "Empresa: ";
+        const nomeEmpresaInput = document.createElement("input");
+        nomeEmpresaInput.setAttribute("type", "text");
+        nomeEmpresaInput.setAttribute("id", "empresa");
+        nomeEmpresaInput.setAttribute("name", "empresa");
+
+        const labelCnpj = document.createElement("label");
+        labelCnpj.setAttribute("for", "cnpj");
+        labelCnpj.textContent = "CNPJ: ";
+        const cnpjInput = document.createElement("input");
+        cnpjInput.setAttribute("type", "text");
+        cnpjInput.setAttribute("id", "cnpj");
+        cnpjInput.setAttribute("name", "cnpj");
+
+        const labelUsuario = document.createElement("label");
+        labelUsuario.setAttribute("for", "usuario");
+        labelUsuario.textContent = "UsuarioID: ";
+        const usuarioInput = document.createElement("input");
+        usuarioInput.setAttribute("type", "text");
+        usuarioInput.setAttribute("id", "usuario");
+        usuarioInput.setAttribute("name", "usuario");
+
+        formulario.appendChild(labelEmpresa);
+        formulario.appendChild(nomeEmpresaInput);
+        formulario.appendChild(labelCnpj);
+        formulario.appendChild(cnpjInput);
+        formulario.appendChild(labelUsuario);
+        formulario.appendChild(usuarioInput);
+        document.getElementById("container").appendChild(formulario);
+    }
+
+}
 async function novoRegistro(event) {
     event.preventDefault();
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const response = await fetch(`http://localhost:3333/${tipo}`, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify({ nome, email, senha })
-    });
-    console.log(response);
-    console.log(await response.json());
+
+    if (tipo == 'usuarios') {
+        nome = document.getElementById("nome").value;
+        email = document.getElementById("email").value;
+        senha = document.getElementById("senha").value;
+        const response = await fetch(`http://localhost:3333/${tipo}`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({ nome, email, senha })
+        });
+        console.log(response);
+        console.log(await response.json());
+    }
+    if (tipo == 'empresas') {
+        nomeEmpresa = document.getElementById("empresa").value;
+        cnpj = document.getElementById("cnpj").value;
+        usuarioId = document.getElementById("usuario").value;
+        const response = await fetch(`http://localhost:3333/${tipo}`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({ nomeEmpresa, cnpj, usuarioId })
+        });
+        console.log(response);
+        console.log(await response.json());
+    }
+    alert("Registro cadastrado com sucesso!")
 }
 async function alteraRegistro(event) {
     event.preventDefault();
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const response = await fetch(`http://localhost:3333/${tipo}/${id}`, {
-        method: "put",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify({ nome, email, senha:"1234" })
-    });
-    console.log(response);
+    if (tipo == 'usuarios') {
+        nome = document.getElementById("nome").value;
+        email = document.getElementById("email").value;
+        senha = document.getElementById("senha").value;
+        const response = await fetch(`http://localhost:3333/${tipo}/${id}`, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({ nome, email, senha })
+        });
+        console.log(response);
+    }
+    if (tipo == 'empresas') {
+        nomeEmpresa = document.getElementById("empresa").value;
+        cnpj = document.getElementById("cnpj").value;
+        usuarioId = document.getElementById("usuario").value;
+        const response = await fetch(`http://localhost:3333/${tipo}/${id}`, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({ nomeEmpresa, cnpj, usuarioId })
+        });
+        console.log(response);
+    }
+    alert("Registro alterado com sucesso!")
 }
 async function deletaRegistro(event) {
     event.preventDefault();
@@ -33,20 +143,27 @@ async function deletaRegistro(event) {
         method: "delete"
     });
     console.log(response);
+    alert("Registro excluído com sucesso!")
 }
 async function buscaId() {
-
     const response = await fetch(`http://localhost:3333/${tipo}/${id}`);
     const dados = await response.json();
-    const nomeInput = document.getElementById("nome");
-    const emailInput = document.getElementById("email");
+
     if (tipo == 'usuarios') {
+        const nomeInput = document.getElementById("nome");
+        const emailInput = document.getElementById("email");
+        const senhaInput = document.getElementById("senha");
         nomeInput.value = dados.nome;
         emailInput.value = dados.email;
+        senhaInput.value = dados.senha;
     }
     if (tipo == 'empresas') {
-        nomeInput.value = dados.nome;
-        emailInput.value = dados.email;
+        const nomeEmpresaInput = document.getElementById("empresa");
+        const cnpjInput = document.getElementById("cnpj");
+        const usuarioInput = document.getElementById("usuario");
+        nomeEmpresaInput.value = dados.nomeEmpresa;
+        cnpjInput.value = dados.cnpj;
+        usuarioInput.value = dados.usuarioId;
     }
 
     console.log(dados);
@@ -59,7 +176,8 @@ document.addEventListener("DOMContentLoaded", function () {
     id = param;
     const paramTipo = params.get("tipo");
     tipo = paramTipo;
-
+    console.log(tipo);
+    montaFormulario(tipo);
     buscaId();
 
 });
